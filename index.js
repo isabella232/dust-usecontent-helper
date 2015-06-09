7e9497101478f17d67f8a955a70afd903ffecb0a
Aria Stewart <aredridel@dinhe.net>
2015-06-09 17:28:56 -0400

    Remove the hackery again, it belongs in dust-makara-helpers since it only works at template registration time anyway

--- index.js
+++ index.js
@@ -25,43 +25,11 @@ module.exports = function (lookup) {
                     chunk.setError(err);
                 } else {
                     ctx = ctx.push({ intl: { messages: content, bundle: params.bundle } });
-                    hackGibson(ctx, content, params.bundle);
                     bodies.block(chunk, ctx).end();
                 }
             });
         });
     }
 };
-
-function hackGibson(ctx, content, bundle) {
-    var oldShiftBlocks = ctx.shiftBlocks;
-    var oldPush = ctx.push;
-    ctx.shiftBlocks = function(locals) {
-        return oldShiftBlocks.call(this, objMap(locals, function (l) {
-            return wrapBlock(l, content, bundle);
-        }));
-    };
-
-    ctx.push = function(head, idx, len) {
-        var newCtx = oldPush.apply(this, arguments);
-        hackGibson(newCtx, content, bundle);
-        return newCtx;
-    };
-}
-
-function wrapBlock(block, content, bundle) {
-    return function (chunk, ctx) {
-        ctx = ctx.push({intl: { messages: content, bundle: bundle }});
-        return block(chunk, ctx);
-    }
-}
-
-function objMap(obj, fn) {
-    var n = {};
-    Object.keys(obj).forEach(function (e) {
-        n[e] = fn(obj[e]);
-    });
-    return n;
-}
 
 module.exports.withLoader = module.exports;
